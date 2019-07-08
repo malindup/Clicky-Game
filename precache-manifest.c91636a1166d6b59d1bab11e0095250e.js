8dbbbf3193c413e8c27083d29260a7233c0ee076
Malindu <Malindu@MacBook-Pro-3.local>
2019-07-08 05:54:31 -0500

    Updates

---
+++ precache-manifest.c91636a1166d6b59d1bab11e0095250e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d992aa0fc00dc455b1",
+    "url": "/static/css/main.2bfd2354.chunk.css"
+  },
+  {
+    "revision": "41d992aa0fc00dc455b1",
+    "url": "/static/js/main.3e361021.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "3379b6a3cac1ff18323898c4385e3ab2",
+    "url": "/index.html"
+  }
+];
