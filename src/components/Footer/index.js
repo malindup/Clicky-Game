4102b550cc2b457c3e5344593ab080cd85537ef1
Malindu <Malindu@MacBook-Pro-3.local>
2019-07-08 05:50:07 -0500

    working app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        copyright 2019 <img alt="react" src="assets/images/react.svg" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
